d8e3274d9929936fa97e83df03207eeee9407841
Eldybyhtyn <name66437@gmail.com>
2023-03-30 18:13:34 +0300

    parse double with bugs

--- main.c
+++ main.c
@@ -15,7 +15,17 @@ void show_string(uint8_t* string) {
     printf("\n");
 }
 
-
+/*void format_double(double num) {
+    num = 1.2;
+    uint8_t *temp_str;
+    sprintf(temp_str, "%f", num);
+    int is_end = 0;
+     for (;*temp_str != '.';++temp_str, ++new_str) {
+         *new_str = *temp_str;
+     }
+    show_string(new_str);
+    
+}*/
 
 void show_args(uint8_t* types, ...) {
     va_list factor;
@@ -45,7 +55,9 @@ void show_args(uint8_t* types, ...) {
 
 void main() {
     //show_args("dddsr", 1, 2, 3, "dick", 1.2);
-    uint8_t* str;
-    sprintf(str, "%f", 1.2);
+    double hi = 1.2;
+    //format_double(1.2);
+    uint8_t *str;
+    sprintf(str, "%f", hi);
     show_string(str);
 }
