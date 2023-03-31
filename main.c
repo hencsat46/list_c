306eee6e1f152648c8dd1284ee1842c4d8d92890
Eldybyhtyn <name66437@gmail.com>
2023-03-31 13:16:20 +0300

    format double done w/ little bug

--- main.c
+++ main.c
@@ -1,6 +1,8 @@
 #include <stdio.h>
 #include <stdarg.h>
 #include <stdint.h>
+#include <stdlib.h>
+#include "consts.h"
 
 typedef union {
     int digit;
@@ -15,17 +17,45 @@ void show_string(uint8_t* string) {
     printf("\n");
 }
 
-/*void format_double(double num) {
-    num = 1.2;
-    uint8_t *temp_str;
-    sprintf(temp_str, "%f", num);
-    int is_end = 0;
-     for (;*temp_str != '.';++temp_str, ++new_str) {
-         *new_str = *temp_str;
-     }
-    show_string(new_str);
+
+
+void format_double(double num) {
     
-}*/
+    uint8_t *old_str = (uint8_t *)malloc(64);
+    
+    sprintf(old_str, "%f", num);
+    printf("%s\n", old_str);
+    bool dot_state = FALSE, find_zero = FALSE;
+    uint8_t *ptr_null;
+    
+    for (uint8_t *ptr = old_str; *ptr; ++ptr) {
+        if (*ptr == '0') {
+            if (!find_zero) {
+                ptr_null = ptr;
+                find_zero = TRUE;
+            }
+        } else {
+            find_zero = FALSE;
+        }
+        
+        
+    }
+    printf("%d\n", find_zero);
+    uint8_t *new_str;
+    if (find_zero) {
+        
+        
+        new_str = (uint8_t *)malloc((size_t)(ptr_null - old_str));
+        for (uint8_t *ptr = old_str, *new_ptr = new_str; ptr < ptr_null; ++ptr, ++new_ptr) {
+            *new_ptr = *ptr;
+        }
+        show_string(new_str);
+    }
+    free(new_str);
+    free(old_str);
+    
+    
+}
 
 void show_args(uint8_t* types, ...) {
     va_list factor;
@@ -55,9 +85,8 @@ void show_args(uint8_t* types, ...) {
 
 void main() {
     //show_args("dddsr", 1, 2, 3, "dick", 1.2);
-    double hi = 1.2;
-    //format_double(1.2);
-    uint8_t *str;
-    sprintf(str, "%f", hi);
-    show_string(str);
+    
+    format_double(144.11);
+    //printf("%.2f\n", 0.2 + 0.3);
+
 }
